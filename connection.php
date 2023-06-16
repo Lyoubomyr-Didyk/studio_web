f26e93ce0c35b50529a1ee73c284a5b45b4fc985
Lyoubomyr-Didyk <lyoubomyr.didyk@le-campus-numerique.fr>
2023-06-16 16:59:57 +0200

    add request

--- connection.php
+++ connection.php
@@ -5,10 +5,17 @@ $dbname = 'fuel-dataviz';
 $username = 'postgres';
 $password = 'password';
 
-$dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
+$dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";  // data source name
 try{
     $conn = new PDO($dsn);
 }catch (PDOException $e){
     echo $e->getMessage();
 }
+
+function request ($conn, $query){
+    $result = $conn->query($query);
+    $lines = $result->fetchAll(PDO::FETCH_ASSOC);
+    return $lines;
+}
 ?>
+
--- index.php
+++ index.php
@@ -22,16 +22,20 @@ require "blocks/header.php";
 ?>
 <?php
  require "connection.php";
+
+/*--------------------------------------------------- 1 --------------------------------------------------------------*/
+
 $result = $conn->query("SELECT DISTINCT adresse, code_postal, ville, service.nom FROM point_de_vente
 JOIN service_point_de_vente spdv on point_de_vente.id = spdv.point_de_vente_id
 JOIN service on spdv.service_id = service.id
-WHERE service.nom LIKE '%gaz%' LIMIT 10;");
+WHERE service.nom LIKE '%gaz%' LIMIT 5;");
 $lines = $result->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <div class="container">
   <table class='m-auto table table-striped'>
+    <p class='mb-2'>#1 Service vente de gaz</p>
     <thead class=''>
-    <tr class='bg-success'>
+    <tr class='bg-warning'>
       <td class='px-5'>CP</td>
       <td class='px-5'>Ville</td>
       <td class='px-5'>Adresse</td>
@@ -50,6 +54,123 @@ $lines = $result->fetchAll(PDO::FETCH_ASSOC);
     </tbody>
   </table>
 </div>
+
+<!------------------------------------------------------- 2 ----------------------------------------------------------->
+<?php
+$result = $conn->query("SELECT * FROM point_de_vente WHERE automate_24_24 = true AND ville = 'ANNECY';");
+$lines = $result->fetchAll(PDO::FETCH_ASSOC);
+?>
+<div class="container">
+  <p class='mt-5 mb-2'>#2 Service automate 24/24</p>
+  <table class='m-auto table table-striped'>
+    <thead class=''>
+    <tr class='bg-success'>
+      <td class='px-5'>CP</td>
+      <td class='px-5'>Ville</td>
+      <td class='px-5'>Adresse</td>
+    </tr>
+    </thead>
+    <tbody class=''>
+    <?php foreach ($lines as $line): ?>
+<tr class=''>
+  <td class='px-5'><?php echo $line['code_postal']; ?></td>
+  <td class='px-5'><?php echo $line['ville']; ?></td>
+  <td class='px-5'><?php echo $line['adresse']; ?></td>
+</tr>
+<?php endforeach; ?>
+</tbody>
+</table>
+</div>
+<!------------------------------------------------------- 3 ----------------------------------------------------------->
+
+<?php
+$query = "SELECT count(*) as nombre FROM point_de_vente WHERE (code_postal  LIKE '29%' OR code_postal LIKE '23%' OR code_postal LIKE '69%');";
+$result = request($conn, $query);
+?>
+
+<div class='container mt-5 row'>
+  <p class='mb-2'>#3 Le nombre de stations dans le département 29, 23 et 69</p>
+  <table class="table table-dark table-striped col-12">
+    <tbody>
+    <tr>
+       <td class='px-5'><?php echo ($result[0]['nombre']);?></td>
+    </tr>
+    </tbody>
+  </table>
+</div>
+
+<!------------------------------------------------------ 4 ------------------------------------------------------------>
+<?php
+$query = "SELECT CAST(avg(valeur) AS numeric(10,3)) FROM  prix
+JOIN carburant c on c.id = prix.carburant_id
+WHERE  date > '2006-12-31 0:00:00' AND date <= '2007-12-31 23:59:59' AND c.nom = 'Gazole';";
+$result1 = request($conn, $query);
+
+$query = "SELECT CAST(avg(valeur) AS numeric(10,3)) FROM  prix
+JOIN carburant c on c.id = prix.carburant_id
+WHERE  date > '2013-12-31 0:00:00' AND date <= '2014-12-31 23:59:59' AND c.nom = 'Gazole';";
+$result2 = request($conn, $query);
+
+$query = "SELECT CAST(avg(valeur) AS numeric(10,3)) FROM  prix
+JOIN carburant c on c.id = prix.carburant_id
+WHERE  date > '2022-12-31 0:00:00' AND date <= '2023-12-31 23:59:59' AND c.nom = 'Gazole';";
+$result3 = request($conn, $query);
+?>
+<div class='container mt-5'>
+  <p class='mb-2'>#4 Prix moyenne du Gazole en France:</p>
+<table class="table">
+  <thead>
+  <tr>
+    <th scope="col">Année</th>
+    <th scope="col">2007</th>
+    <th scope="col">2014</th>
+    <th scope="col">2023</th>
+  </tr>
+  </thead>
+  <tbody>
+  <tr>
+    <th scope="row">Prix</th>
+    <td><?php echo ($result1[0]["avg"]);?></td>
+    <td><?php echo ($result2[0]["avg"]);?></td>
+    <td><?php echo ($result3[0]["avg"]);?></td>
+  </tr>
+  </tbody>
+</table>
+</div>
+<!------------------------------------------------------ 5 ------------------------------------------------------------>
+<?php
+$query = "SELECT LEFT(code_postal, 2) AS departement, CAST(avg(valeur) AS numeric(10,3)) AS prix_moyenne FROM point_de_vente
+JOIN prix ON point_de_vente.id = prix.point_de_vente_id
+JOIN carburant ON prix.carburant_id = carburant.id
+WHERE carburant.nom = 'Gazole'
+GROUP BY departement
+ORDER BY departement;";
+$results = request($conn, $query);
+?>
+<div class='container'>
+  <p class='mt-5 mb-2'>#5 Prix moyenne du Gazole par département:</p>
+<table class="table table-info">
+  <thead>
+  <tr>
+    <th scope="col">Departement</th>
+    <th scope="col">Prix moyenne</th>
+  </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($results as $result): ?>
+  <tr>
+    <td><?php echo $result['departement'] ; ?></td>
+    <td><?php echo $result['prix_moyenne'] ." €"; ?></td>
+  </tr>
+  <?php endforeach; ?>
+  </tbody>
+</table>
+</div>
+
+<!------------------------------------------------------ 6 ------------------------------------------------------------>
+
+
+<!--------------------------------------------------------------------------------------------------------------------->
 
 <?php
 if(isset($_GET['url']) && $_GET['url'] == "portfolio" ){
